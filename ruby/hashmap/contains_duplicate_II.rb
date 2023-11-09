8643656605b75be6dae65bd170d2b1d93f36551e
Cody <codybarker.or@gmail.com>
2023-11-08 19:43:10 -0800

    solve happy number

---
+++ ruby/hashmap/contains_duplicate_II.rb
@@ -0,0 +1,17 @@
+
+nums = [1,2,3,2,4]
+k = 2
+
+def contains_nearby_duplicate(nums, k)
+  hash = {} # key: num and value: index
+  (0...nums.length).each do |i|
+      if hash[nums[i]]
+          if (i - hash[nums[i]]).abs <= k
+              return true
+          end
+      end
+      hash[nums[i]] = i
+  end
+  false
+end
+
