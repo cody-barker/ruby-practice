04a8d390935177730b33efb461c56d409d5c8f5a
Cody <codybarker.or@gmail.com>
2023-11-09 18:38:04 -0800

    add mass_assignment.rb

---
+++ ruby/mass_assignment.rb
@@ -0,0 +1,14 @@
+class User
+  attr_accessor :name, :email
+
+  def initialize(params)
+    @name = params[:name]
+    @email = params[:email]
+  end
+end
+
+# Mass assignment during object creation
+user = User.new(email: "john@example.com", name: "John Doe")
+
+puts user.name  # Outputs: John Doe
+puts user.email # Outputs: john@example.com
