26928401cf39ebbe4c45b41b23b1bc458ab5f9bb
Cody <codybarker.or@gmail.com>
2023-11-15 15:15:43 -0800

    add alterative two sum method

--- ruby/hashmap/two_sum.rb
+++ ruby/hashmap/two_sum.rb
@@ -1,20 +1,35 @@
 require 'pry'
 
-nums = [1,2,3,5,8]
+nums = [1,2,3,8,5]
 target = 13
 
-def two_sum(nums, target)
+# def two_sum(nums, target)
+#   nums.each_with_index do |first_number, first_index|
+#     second_number = target - first_number
+
+#     second_index = nums.index(second_number)
+
+#     # binding.pry
+
+#     next if second_index.nil? || second_index == first_index
+
+#     return [first_index, second_index]
+#   end
+# end
+
+# p two_sum(nums, target)
+
+def two_sums(nums, target)
   nums.each_with_index do |first_number, first_index|
     second_number = target - first_number
 
     second_index = nums.index(second_number)
 
-    binding.pry
-
-    next if second_index.nil? || second_index == first_index
-
-    return [first_index, second_index]
+    if second_index && first_index != second_index
+      return [first_index, second_index]
+    end
   end
+  nil
 end
 
-p two_sum(nums, target)
+p two_sums(nums, target)
