2a3c3d00180ebc353601d4c554b67146e384c556
Cody <codybarker.or@gmail.com>
2023-11-17 14:12:05 -0800

    add additional palindrome method

--- ruby/palindrome.rb
+++ ruby/palindrome.rb
@@ -1,16 +1,31 @@
 
-def palindrome?(string)
-  if string.length == 1 || string.length == 0
-    true
-  else
-    if string[0] == string[-1]
-      palindrome?(string[1..-2])
-    else
-      false
-    end
-  end
-end
+# def palindrome?(string)
+#   if string.length == 1 || string.length == 0
+#     true
+#   else
+#     if string[0] == string[-1]
+#       palindrome?(string[1..-2])
+#     else
+#       false
+#     end
+#   end
+# end
 
-p palindrome?("aaazzaaa")
+# p palindrome?("aaazzaaa")
 
 # this might have more overhead due to the repeated function calls and string slicing, which for very long strings could lead to a stack overflow. Perhaps there's a better way to do this that's logarthmic but not recursive?
+
+def palindrome?(string)
+  if string[0] != string[-1]
+    return false
+  end
+  i = string.length - 1
+  reversed_string = ""
+  while i >= 0
+    reversed_string << string[i]
+    i -= 1
+  end
+  reversed_string == string
+end
+
+p palindrome?("racecar")
