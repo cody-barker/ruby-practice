0c10cc0d50384fd37edba6764ad1d1f427937b0f
Cody <codybarker.or@gmail.com>
2023-11-17 14:18:02 -0800

    add new attempt at binary_search method

--- ruby/binary_search.rb
+++ ruby/binary_search.rb
@@ -1,23 +1,44 @@
 arr = [1,2,5,7,9,13,20,21,27,30]
 target = 13
 
-def binary_search(arr, target)
-  min = 0
-  max = arr.length - 1
+# def binary_search(arr, target)
+#   min = 0
+#   max = arr.length - 1
 
+#   while min <= max
+#     mid = (min + max) / 2
+#     if arr[mid] == target
+#       return mid
+#     elsif arr[mid] > target
+#       max = mid - 1
+#     else
+#       min = mid + 1
+#     end
+#   end
+
+#   return nil
+
+# end
+
+# p binary_search([1,13,12,10,23,15,7,8].sort, target) ## should equal 5
+# p binary_search(arr, target) ## should equal 5
+# p binary_search(arr, 11)
+
+def binary_search(array, target)
+  min = 0
+  max = array.length - 1
+  
   while min <= max
     mid = (min + max) / 2
-    if arr[mid] == target
-      return mid
-    elsif arr[mid] > target
+    if array[mid] == target
+      return array[mid]
+    elsif array[mid] > target
       max = mid - 1
     else
       min = mid + 1
     end
   end
-
   return nil
-
 end
 
 p binary_search([1,13,12,10,23,15,7,8].sort, target) ## should equal 5
