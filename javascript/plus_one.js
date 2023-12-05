3fa673a127bfb450127ef85cfdaa54eb1aa7a2e9
Cody <codybarker.or@gmail.com>
2023-12-05 14:29:31 -0800

    add complete answer to plus_one

--- javascript/plus_one.js
+++ javascript/plus_one.js
@@ -1,19 +1,35 @@
-const nums = [9]
+const nums = [1,2,3,4]
 
+
+// var plusOne = function(digits) {
+//   if (digits[digits.length - 1] !== 9) {
+//       digits[digits.length - 1] = (digits[digits.length - 1] + 1)
+//   } else {
+//     if (digits.length === 1) {
+//       digits[digits.length - 1] = 0
+//       digits.unshift(1)
+//     } else {
+//       digits[digits.length - 1] = 0
+//       digits[digits.length - 2] = (digits[digits.length - 2] + 1)
+//     }
+//   }
+//   return digits
+// };
 
 var plusOne = function(digits) {
-  if (digits[digits.length - 1] !== 9) {
-      digits[digits.length - 1] = (digits[digits.length - 1] + 1)
-  } else {
-    if (digits.length === 1) {
-      digits[digits.length - 1] = 0
-      digits.unshift(1)
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] !== 9) {
+      digits[i]++;
+      return digits;
     } else {
-      digits[digits.length - 1] = 0
-      digits[digits.length - 2] = (digits[digits.length - 2] + 1)
+      digits[i] = 0;
     }
   }
-  return digits
+
+  // If we are here, it means we need to add a new digit
+  digits.unshift(1);
+
+  return digits;
 };
 
 console.log(plusOne(nums))
